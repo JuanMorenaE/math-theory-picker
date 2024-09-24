60ea4f73d76176f41484aa7045e5c1ddaf7a2d20
Juan Morena <juanmorenae@gmail.com>
2024-09-24 17:48:12 -0300

    Temas Primer Parcial Analisis

--- analisis/script.js
+++ analisis/script.js
@@ -1,23 +1,28 @@
 const theoryList = [
-  "Criterio del Cociente de D'Alembert y Cauchy.",
-  "Existencia y unicidad de soluciones en ecuaciones diferenciales.",
-  "Condición necesaria de convergencia.",
-  "Teorema fundamental del cálculo integral.",
-  "Raíz n-ésima en números complejos.",
-  "Integración por partes y por sustitución para primitivas.",
-  "e^z = 1 => z = 2kπi y z = 2kπi => e^z = 1",
-  "e^z = e^w => z = w + 2kπi y z = w + 2kπi => e^z = e^w",
-  "Primer Criterio de Comparación de impropias de primera especie.",
-  "Demostrar que el conjunto solución de una ecuación diferencial lineal homogénea de segundo orden o orden n es subespacio y su dimensión.",
-  "Convergencia Dominada y Convergencia Absoluta.",
+  "Demostrar que si f es diferenciable en p entonces f es continua en p.",                                                                                // Diferenciabilidad
+  "Demostrar que si f es diferenciable en p entonces existe df/dv(p) para todo v que pertenece a R^n no nulo y df/dv(p) = diferencial de f en p (v).",    // Diferenciabilidad
+  "Enuncie y demuestre la condición suficiente de diferenciabilidad.",                                                                                    // Diferenciabilidad
+  "Enuncie y demuestre la condición suficiente de diferenciabilidad para el caso de una función de dos variables.",                                       // Diferenciabilidad
+
+  "Demostrar que || || domina a || ||\'.",                                                                                                                // Normas
+  "Demostrar que toda bola de centro 0 segun || ||\' contiene una bola del mismo centro segun || ||.",                                                    // Normas
+  "Demostrar que toda bola segun || ||\' contiene una bola del mismo centro segun || ||.",                                                                // Normas
+  "Demostrar que las tres normas usuales son equivalentes.",                                                                                              // Normas
+  "Defina normas equivalentes.",                                                                                                                          // Normas
+  "Defina norma en Rn.",                                                                                                                                  // Normas
+
+  "Sean A y B dos subconjuntos abiertos de Rn, pruebe que C = A u B es un conjunto abierto.",                                                             // Conjuntos
+  "Sean A y B dos subconjuntos abiertos de Rn, pruebe que C = A n B es un conjunto abierto.",                                                             // Conjuntos
+
+  "Enuncie y demuestre la regla de la cadena.",                                                                                                           // Regla de la Cadena
 ]
 
 let learnedList = []
 
-window.addEventListener('load',(() => {
-    learnedList = JSON.parse(localStorage.getItem('learned'))
-    if (!learnedList) learnedList = []
-    showTheoryData()
+window.addEventListener('load', (() => {
+  learnedList = JSON.parse(localStorage.getItem('learned'))
+  if (!learnedList) learnedList = []
+  showTheoryData()
 }))
 
 const showTheoryData = (() => {
@@ -29,46 +34,46 @@ const showTheoryData = (() => {
   theoryContainer.innerHTML = ''
   learnedContainer.innerHTML = ''
 
-  for (theory of theoryList){
+  for (theory of theoryList) {
     let listItem = document.createElement('li')
     listItem.innerHTML = `${theory}`
     listItem.classList.add('theory-item')
     listItem.setAttribute('data-theory', theory)
 
-    if(learnedList){
-      if(!learnedList.includes(theory)){
+    if (learnedList) {
+      if (!learnedList.includes(theory)) {
         listItem.style.color = '#333'
-      }else{
+      } else {
         listItem.style.color = '#aaa'
         listItem.style.textDecoration = 'line-through'
       }
     }
 
     listItem.addEventListener('click', (() => {
-      if(!learnedList || (learnedList && !learnedList.includes(listItem.getAttribute('data-theory')))){
+      if (!learnedList || (learnedList && !learnedList.includes(listItem.getAttribute('data-theory')))) {
         learnedList.push(listItem.getAttribute('data-theory'))
       }
       showTheoryData()
     }))
-    
+
     theoryContainer.append(listItem)
   }
 
   leftTheory.innerText = theoryList.length - learnedList.length
   leftLearned.innerText = learnedList.length
 
-  if(learnedList){
-    for (theory of learnedList){
+  if (learnedList) {
+    for (theory of learnedList) {
       let listItem = document.createElement('li')
       listItem.innerHTML = `${theory}`
       listItem.classList.add('theory-item')
       listItem.setAttribute('data-theory', theory)
-  
+
       listItem.addEventListener('click', (() => {
         learnedList.splice(learnedList.indexOf(listItem.getAttribute('data-theory')), 1)
         showTheoryData()
       }))
-  
+
       learnedContainer.append(listItem)
     }
   }
@@ -79,13 +84,13 @@ const showTheoryData = (() => {
 
 const randomPick = (() => {
   const random = document.getElementById('random')
-  
-  if(learnedList.length == 0){
+
+  if (learnedList.length == 0) {
     random.innerText = ""
     return
   }
-  
+
   randomNumber = Math.floor(Math.random() * (learnedList.length) + 1)
   random.innerText = learnedList[randomNumber - 1]
-  
+
 })
